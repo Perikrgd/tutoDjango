23be38538dec5c949defe435e50abd4fcc57aeb9
Rigoudy <p.rigoudy.sin@gmail.com>
2020-09-25 15:58:33 +0200

    view generic

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
+
+    # exemple: /polls/
+    path('', views.IndexView.as_view(), name='index'),
+
+    # exemple: /polls/specifices/5/    specifices est ajouter pour que l'url soit plus logique à comprendre mais il n'est pas nécessaire
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+
+    # exemple: /polls/5/results/
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    
+    # exemple: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
